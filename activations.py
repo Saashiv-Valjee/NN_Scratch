9eede3f49c27028dcd47182717dcc003b68b814b
saashiv <saashivvaljee@hotmail.co.uk>
2025-08-04 19:51:33 +0200

    activations

---
+++ activations.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def sigmoid(z):
+    # good for binary classification
+    return 1 / (1 + np.exp(-z))
+
+def sigmoid_derivative(z):
+    # the derivative for gradient descent
+    s = sigmoid(z)
+    return s * (1 - s)
+
+def relu(z):
+    # good for hidden layers due to easy computation and not scaling inputs to high values
+    return np.maximum(0, z)
+
+def relu_derivative(z):
+    # derivative is 1 if x > 0, which is set to 1 otherwise 0
+    return (z > 0).astype(float)
+
