a617f1b221746d5fb97e2d4696997db7aaf7f7b9
Saashiv-valjee <saashivvaljee@hotmail.co.uk>
2025-08-05 00:30:54 +0100

    back propogation maths and network class finished

--- NN.py
+++ NN.py
@@ -44,7 +44,9 @@ class NeuralNet:
     
     def loss(self,y):
         # need some kind of classification based loss function.
-        # binary cross entropy is used most often 
+        # binary cross entropy is used most often due to it being "convex" (less chance to fall into local minima?)
+        # but we will use 
+        # this function will need the classification of the model and the truth labels y
         
         # we can use the current sample size to scale the loss, otherwise loss would scale up with increasing data size
         # y will be the matrix of training labels, with the columns being the amount of training data samples 
@@ -65,26 +67,37 @@ class NeuralNet:
         # by doing this, we can change the weights and check if they cause the loss function gradient to increase or decrease
         # a decreasing loss function means we are improving the learning and we should continue changing the weights in the direction we are currently 
 
-        # going forward we have 
-        # W1B1 -> Z1 (dot)
-        # Z1   -> A1 (relu)
-        # A1B2 -> Z2 (dot)
-        # Z2   -> A2 (sig)
-
-        # so we need to go back via 
-        # A2 -> Z2   d(sig):    A2 - y
-        # Z2 -> A1B2 d(weight): 
-
-        # continue on paper, use 
-        # https://medium.com/@waadlingaadil/learn-to-build-a-neural-network-from-scratch-yes-really-cac4ca457efc
-        # for aid
-        # go make ur pasta
+        # this will be explained in depth elsewhere in the repo
 
         # learning rate corresponds to the amount by which initial weights are altered
         
         # X is the training data, of dimensions [features,samples]
         m = X.shape[1]
-        
-        dZ2 = self.A2 - y
+
+        # === Gradients for output layer ===
+        # dL/dW2 = dL/dA2 · dA2/dZ2 · dZ2/dW2  = (A2 - y) · A1^T     (from chain rule + vectorization)
+        dL_dW2 = (1/m) * np.dot(self.A2 - y, self.A1.T)
+        # dL/db2 = dL/dZ2 · dZ2/db2 = sum over samples of dZ2
+        dL_db2 = (1/m) * np.sum(self.A2 - y, axis=1, keepdims=True)
 
 
+        # === Gradients for hidden layer ===
+        # dL/dZ2 = dL/dA2 · dA2/dZ2 = A2 - y
+        dL_dZ2 = self.A2 - y
+        # dL/dA1 = dL/dZ2 · dZ2/dA1 = W2^T · dZ2
+        dL_dA1 = np.dot(self.W2.T, dL_dZ2)
+        # dL/dZ1 = dL/dA1 · dA1/dZ1 = dA1 * ReLU'(Z1)
+        dL_dZ1 = dL_dA1 * relu_derivative(self.Z1)
+        # dL/dW1 = dL/dZ1 · dZ1/dW1 = dZ1 · X^T
+        dL_dW1 = (1/m) * np.dot(dL_dZ1, X.T)
+        # dL/db1 = dL/dZ1 · dZ1/db1 = sum over samples of dZ1
+        dL_db1 = (1/m) * np.sum(dL_dZ1, axis=1, keepdims=True)
+
+
+        # Gradient descent update
+        # change the weights based on the change in the gradient wrt the weights
+        # if the gradient is positive, move the weight one direction +/-, if it's negative go the other way.
+        self.W1 = self.W1 - (learning_rate * dL_dW1)
+        self.b1 = self.b1 - (learning_rate * dL_db1)
+        self.W2 = self.W2 - (learning_rate * dL_dW2)
+        self.b2 = self.b2 - (learning_rate * dL_db2)
