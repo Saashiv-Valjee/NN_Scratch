3a07e5ad90844f62be0e0f8ddadf1bd6f8cbe123
saashiv <saashivvaljee@hotmail.co.uk>
2025-08-04 21:46:20 +0200

    forward feeding and loss

--- NN.py
+++ NN.py
@@ -26,4 +26,35 @@ class NeuralNet:
         self.W2 = np.random.randn(1, hidden_size) * 0.01
         self.b2 = np.zeros((1, 1))
 
+    
+    def forward(self,X):
+        # forward pass / feed forward for computations 
+        # comptuations are the dot product of the initial signals from previous layers + the weights on the edges
+        # passed through the layers corresponding activation function 
+
+        # layer 1 - Hidden layer
+        self.Z1 = np.dot(self.W1,X) + self.b1
+        self.A1 = relu(self.Z1)
+
+        # Layer 2 - Output Layer 
+        self.Z2 = np.dot(self.W2,self.A1) + self.b2
+        self.A2 = sigmoid(self.Z2)
+
+        return self.A2
+    
+    def loss(self,y):
+        # need some kind of classification based loss function.
+        # binary cross entropy is used most often 
         
+        # we can use the current sample size to scale the loss, otherwise loss would scale up with increasing data size
+        # y will be the matrix of training data, with the columns being the amount of training data samples 
+        m = y.shape[1]
+
+        # if the network predicts exactly 0 or 1 (self.A2), the below will crash
+        # we need to circumvent this using some negligable offset
+        eps = 1e-15  
+
+        # binary cross entropy
+        return -np.mean(y * np.log(self.A2 + eps) + (1 - y) * np.log(1 - self.A2 + eps))
+
+
